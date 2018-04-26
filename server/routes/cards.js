0e85b135f8c1ec4db09b7498bed7dcb54f03ab91
CCWindy <843905832@qq.com>
2018-04-26 23:26:52 +0800

    Allowed multiple Access-Control-Allow-Origin value

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -3,9 +3,11 @@ var router = express.Router();
 
 /* GET cards listing. */
 router.get('/', function (req, res, next) {
-    res.set({
-        'Access-Control-Allow-Origin': 'http://localhost'
-    });
+    var allowedOrigins = ['http://locaohost', 'http://localhost:4200'];
+    var origin = req.headers.origin;
+    if (allowedOrigins.indexOf(origin) > -1) {
+        res.set('Access-Control-Allow-Origin', origin);
+    }
     global.db.collection("cards").find({name: "Crowd Favorite"}).toArray(function (err, result) { // 返回集合中所有数据
         if (err) throw err;
         console.log("成功连接cards");
