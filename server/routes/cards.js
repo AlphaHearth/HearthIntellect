28197f096bec327979c9545e61e86a7f334e4904
CCWindy <843905832@qq.com>
2018-05-16 23:05:17 +0800

    Allowed getting cards with page & pageSize

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -3,12 +3,14 @@ var router = express.Router();
 
 /* GET cards listing. */
 router.get('/', function (req, res, next) {
-    var allowedOrigins = ['http://locaohost', 'http://localhost:4200'];
+    var allowedOrigins = ['http://localhost', 'http://localhost:4200'];
     var origin = req.headers.origin;
     if (allowedOrigins.indexOf(origin) > -1) {
         res.set('Access-Control-Allow-Origin', origin);
     }
-    global.db.collection("cards").find({name: "Crowd Favorite"}).toArray(function (err, result) { // 返回集合中所有数据
+    var page = req.query.page ? req.query.page : 1;
+    var pageSize = req.query.pageSize ? req.query.pageSize : 10;
+    global.db.collection("cards").find().skip((page - 1) * pageSize).limit(pageSize).toArray(function (err, result) { // 返回集合中所有数据
         if (err) throw err;
         console.log("成功连接cards");
         console.log(result);
