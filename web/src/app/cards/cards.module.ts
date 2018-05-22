a73c26b61b93132ed18dfa25594f5c0e4e41b29d
CCWindy <843905832@qq.com>
2018-05-22 12:49:34 +0800

    Allowed turning page

--- web/src/app/cards/cards.module.ts
+++ web/src/app/cards/cards.module.ts
@@ -5,7 +5,7 @@ import {HttpClientModule} from '@angular/common/http';
 import {CardsService} from "./cards.service";
 import {CardListComponent} from "./cardList/cardList.component";
 import {CardComponent} from "./card/card.component";
-import {MatCardModule, MatChipsModule} from "@angular/material";
+import {MatCardModule, MatChipsModule, MatPaginatorModule} from "@angular/material";
 
 
 @NgModule({
@@ -14,8 +14,8 @@ import {MatCardModule, MatChipsModule} from "@angular/material";
     BrowserModule,
     HttpClientModule,
     MatCardModule,
-    MatChipsModule
-  ],
+    MatChipsModule,
+    MatPaginatorModule],
   providers: [CardsService]
 })
 export class CardsModule {
--- web/src/app/cards/cards.service.ts
+++ web/src/app/cards/cards.service.ts
@@ -1,13 +1,35 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 
+export interface Card {
+  id: string,
+  name: string,
+  playerClass: string,
+  rarity: string,
+  set: string,
+  text: string,
+  type: string
+}
+
 @Injectable()
 export class CardsService {
+  public static host = `http://localhost:4000`;
 
-    constructor(private httpClient: HttpClient) {
-    }
+  constructor(private httpClient: HttpClient) {
+  }
 
-    public getCards(){
-        return this.httpClient.get("http://localhost:4000/cards");
+  public getCards(searchParam?: { [key: string]: string }) {
+    const url = this.getUrl('../cards/',searchParam )
+    return this.httpClient.get(url);
+  }
+
+  public getUrl(path: string, searchParam: { [key: string]: string }): string {
+    const url = new window.URL(path, CardsService.host);
+    for (let key in searchParam) {
+      if (searchParam[key]) {
+        url.searchParams.set(key, searchParam[key]);
+      }
     }
+    return url.toString();
+  }
 }
