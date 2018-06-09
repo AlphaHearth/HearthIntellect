f645ec97a03d14a2e2a6635a8900c2bd2cd45ba6
CCWindy <843905832@qq.com>
2018-06-09 23:57:47 +0800

    Fixed error handler

--- server/app.js
+++ server/app.js
@@ -31,11 +31,8 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-    // render the error page
-    res.status(err.status || 500);
+    console.log(err);
+    res.status(500).send({error: err.toString()});
 });
 
 module.exports = app;
